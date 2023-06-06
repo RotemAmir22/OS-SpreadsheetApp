e3ccaca7ece3be635e3e18c608f7a09e6607d580
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-06-06 13:38:00 +0300

    Design

--- SpreadsheetApp/Form1.Designer.cs
+++ SpreadsheetApp/Form1.Designer.cs
@@ -41,11 +41,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.85714F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(589, 23);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(603, 67);
+            this.label1.Size = new System.Drawing.Size(840, 106);
             this.label1.TabIndex = 0;
             this.label1.Text = "ENCRYPTION CODE";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -65,18 +65,18 @@
             // 
             this.rows_textBox.Location = new System.Drawing.Point(29, 153);
             this.rows_textBox.Name = "rows_textBox";
-            this.rows_textBox.Size = new System.Drawing.Size(290, 37);
+            this.rows_textBox.Size = new System.Drawing.Size(320, 37);
             this.rows_textBox.TabIndex = 2;
             this.rows_textBox.TextChanged += new System.EventHandler(this.rows_textBox_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Font = new System.Drawing.Font("SimSun", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(23, 105);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(248, 36);
+            this.label2.Size = new System.Drawing.Size(603, 64);
             this.label2.TabIndex = 3;
             this.label2.Text = "Enter file number:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -84,10 +84,11 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Font = new System.Drawing.Font("SimSun", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Location = new System.Drawing.Point(29, 332);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(187, 69);
+            this.button1.Size = new System.Drawing.Size(320, 69);
             this.button1.TabIndex = 12;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = false;
@@ -96,10 +97,11 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button2.Font = new System.Drawing.Font("SimSun", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Black;
             this.button2.Location = new System.Drawing.Point(29, 217);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(187, 69);
+            this.button2.Size = new System.Drawing.Size(320, 69);
             this.button2.TabIndex = 13;
             this.button2.Text = "Load";
             this.button2.UseVisualStyleBackColor = false;
@@ -108,10 +110,11 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button3.Font = new System.Drawing.Font("SimSun", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.Black;
             this.button3.Location = new System.Drawing.Point(29, 440);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(187, 69);
+            this.button3.Size = new System.Drawing.Size(320, 69);
             this.button3.TabIndex = 16;
             this.button3.Text = "CAPITALIZE";
             this.button3.UseVisualStyleBackColor = false;
